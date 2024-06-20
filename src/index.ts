a06247df885bb8efe3cb22fe477c92665dbd19d2
José <josegil097@gmail.com>
2024-06-19 22:20:05 -0500

    Removing readline-sync

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
-import { Calculator } from './calculator';
-import * as readlineSync from 'readline-sync';
+import { Calculator } from "./calculator";
+const readline = require("readline");
 
-const instr = 'To calculate, please enter the operation to calculate in the following format:'
-const operationFormat = '<operations> (add | subtract | multiply | divide) <number> <number>'
+const instr =
+  "To calculate, please enter the operation to calculate in the following format:";
+const operationFormat =
+  "<operations> (add | subtract | multiply | divide) <number> <number>";
 
-console.info('Welcome to calculator');
-console.info(instr)
-console.info(operationFormat)
+console.info("Welcome to calculator");
+console.info(instr);
+console.info(operationFormat);
 
 const calculator = new Calculator();
-let running = true;
 
-while (running) {
-  const input = readlineSync.question('= ')
-  const parts = input.trim().split(' ');
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+const readLine = () => {
+  rl.question("= ", (input: string) => {
+    throwOpearation(input);
+  });
+};
+
+readLine();
+
+const throwOpearation = (input: string) => {
+  const parts = input.trim().split(" ");
   const operation = parts[0];
 
-  if (parts.length !== 3 && operation !== 'exit' &&  operation !== 'help') {
-    console.error('Invalid input format. Please enter <operation> <number> <number>');
-    continue;
+  if (parts.length !== 3 && operation !== "exit" && operation !== "help") {
+    console.error(
+      "Invalid input format. Please enter <operation> <number> <number>"
+    );
   }
 
   const a = parseFloat(parts[1]);
@@ -26,33 +37,33 @@ while (running) {
   let result: number;
 
   switch (operation) {
-    case 'add':
+    case "add":
       result = calculator.add(a, b);
       console.log(`Result: ${result}`);
       break;
-    case 'subtract':
+    case "subtract":
       result = calculator.subtract(a, b);
       console.log(`Result: ${result}`);
       break;
-    case 'multiply':
+    case "multiply":
       result = calculator.multiply(a, b);
       console.log(`Result: ${result}`);
       break;
-    case 'divide':
+    case "divide":
       result = calculator.divide(a, b);
       console.log(`Result: ${result}`);
       break;
-    case 'help':
-      console.info('Help...')
-      console.info(instr)
-      console.info(operationFormat)
+    case "help":
+      console.info("Help...");
+      console.info(instr);
+      console.info(operationFormat);
       break;
-    case 'exit':
-      running = false;
-      console.log('Exiting...');
-      break;
+    case "exit":
+      console.log("Exiting...");
+      process.exit(0);
     default:
-      console.error('Unsupported operation');
-      continue;
+      console.error("Unsupported operation");
   }
-}
+
+  readLine();
+};
